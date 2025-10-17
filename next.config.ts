5dc3a574ee2c1fe92438e4ab2c17894b542436a5
Moeein <moeeeinaali@gmail.com>
2025-10-17 12:11:36 +0330

    [FEAT] add arvan storage to next-config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s3.ir-tbz-sh1.arvanstorage.ir"
+            }
+        ]
+    }
 };
 
 export default nextConfig;
