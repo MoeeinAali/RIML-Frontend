68310ee51f9606e1d280006a90241799540c2d0e
Moeein Aali <moeeeinaali@gmail.com>
2025-10-17 22:07:19 +0330

    [FEAT] add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,22 +1,65 @@
-import {Suspense} from "react";
-import {TeamList} from "@/ui/components/team/team-list.component";
+import Link from "next/link";
+import MaterialSymbolsMailOutlineRounded from "@/ui/icons/MaterialSymbolsMailOutlineRounded";
+import MaterialSymbolsLocationOnOutline from "@/ui/icons/MaterialSymbolsLocationOnOutline";
+import {Social} from "@/ui/components/social/social.component";
+import SkillIconsLinkedin from "@/ui/icons/SkillIconsLinkedin";
+import LogosYoutubeIcon from "@/ui/icons/LogosYoutubeIcon";
+import SkillIconsGithubLight from "@/ui/icons/SkillIconsGithubLight";
+import LogosTelegram from "@/ui/icons/LogosTelegram";
 
-export default function TeamPage() {
+export default function ContactPage() {
     return (
         <>
-            <h1 className={"text-4xl font-bold text-neutral-700 mb-2"}>Team Members</h1>
+            <h1 className={"text-4xl font-bold text-neutral-700 mb-2"}>Contact</h1>
             <hr/>
-            <p className={"w-full text-wrap text-justify mt-2 text-neutral-600"}>
-                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque
-                sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
-                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque
-                sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
-                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque
-                sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
-            </p>
-            <Suspense fallback={<p className={"animate-pulse"}>kir</p>}>
-                <TeamList/>
-            </Suspense>
+            <br/>
+
+            <div className={"flex gap-3"}>
+                <MaterialSymbolsMailOutlineRounded className={"text-2xl"}/> <Link href={"mailto:Rohban@sharif.edu"}
+            >Rohban@sharif.edu</Link>
+            </div>
+
+            <br/>
+
+            <div className={"flex gap-3"}>
+                <MaterialSymbolsLocationOnOutline className={"text-2xl"}/> <Link
+                href={"https://maps.app.goo.gl/FLP3ufm7oHoLeQ2r6"}
+                target={'_blank'}>Azadi sreet, Tehran,
+                Iran </Link>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <h2 className={"text-3xl font-semibold text-neutral-700 mb-2"}>Socials</h2>
+            <div className={"flex flex-col lg:flex-row pt-4 gap-4 lg:gap-10 xl:gap-20 2xl:gap-28"}>
+                <Link href={"https://www.linkedin.com/company/robust-and-interpretable-machine-learning-lab/"}
+                      className={"flex gap-3 justify-center items-center hover:opacity-80 text-neutral-700 font-medium"}
+                      target={'_blank'}>
+                    <SkillIconsLinkedin className={"text-2xl"}/>
+                    Linkedin
+                </Link>
+                <Link href={"https://www.youtube.com/@RIMLLAB"}
+                      className={"flex gap-3 justify-center items-center hover:opacity-80 text-neutral-700 font-medium"}
+                      target={'_blank'}>
+                    <LogosYoutubeIcon className={"text-2xl"}/>
+                    Youtube
+                </Link>
+                <Link href={"https://github.com/rohban-lab"}
+                      className={"flex gap-3 justify-center items-center hover:opacity-80 text-neutral-700 font-medium"}
+                      target={'_blank'}>
+                    <SkillIconsGithubLight className={"text-3xl"}/>
+                    Github
+                </Link>
+                <Link href={"https://t.me/RIMLLab"}
+                      className={"flex gap-3 justify-center items-center hover:opacity-80 text-neutral-700 font-medium"}
+                      target={'_blank'}>
+                    <LogosTelegram className={"text-3xl"}/>
+                    Telegram
+                </Link>
+
+            </div>
         </>
     );
 }
